ce8adb621cde32cab55f3438502088aa67e875f3
emreutkan <username@users.noreply.github.com>
2024-01-02 22:48:54 +0300

    LC 9

--- src/LC9PalindromeNumber.java
+++ src/LC9PalindromeNumber.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class LC9PalindromeNumber {
+    class Solution {
+        public boolean isPalindrome(int x) {
+            if(x<0){
+                return false;
+            }
+            String palindrome = String.valueOf(x);
+            int j = palindrome.length()-1;
+            for (int i = 0; i < palindrome.length(); i++) {
+                char a = palindrome.charAt(i);
+                char b = palindrome.charAt(j);
+                if ( a!=b
+                ) {
+                    return false;
+                }
+                j--;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
